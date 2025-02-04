00ed35fbe4d01b7aa35a5dffc4e6c1be980bd956
GeNeSix <nnamdiamaga2k20@gmail.com>
2025-02-04 21:06:55 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import io from 'socket.io-client'
+
+// const userNo = '07043952140';
+
+// const socket = io.connect('http://localhost:3000', {query: {userNo}})
+const socket = io.connect('http://localhost:3000')
+
+
+function App() {
+  const [messageInput, setMessageInput] = useState('')
+  const [messages, setMessages] = useState([])
+  const [room, setRoom] = useState(null);
+  const [roomInput, setRoomInput] = useState('');
+
+  const sendMessage = () => {
+    const data = {message: messageInput, user: 'other', room: room};
+    setMessages((prev) => {
+      let md = [...prev, {message: messageInput, user: 'me', room: room}]
+
+      localStorage.setItem('gxmessages', JSON.stringify(md))
+
+      return md
+    })
+    socket.emit('sendMessageToRoom', data)
+  }
+
+  const joinRoom = () => {
+    if(!isNaN(roomInput)){
+      socket.emit('joinRoom', roomInput)
+      setRoom(roomInput);
+    }
+  }
+
+  useEffect(() => {
+    // if(!localStorage && !localStorage.getItem('gxmessages')) return;
+
+    if(localStorage.getItem('gxmessages')){
+      let ms = JSON.parse(localStorage.getItem('gxmessages'));
+      let empty = [];
+      Object.values(ms).forEach(value => {
+        empty.push(value);
+      })
+
+      setMessages((prev) =>  [...empty])
+    }
+  }, [])
+
+  useEffect(() => {
+    const handleAddMessage = (data) => {
+      // setMessages((prev) =>  [...prev, data])
+      setMessages((prev) => {
+        let md = [...prev, data]
+  
+        localStorage.setItem('gxmessages', JSON.stringify(md))
+  
+        return md
+      })
+    }
+    socket.on("receiveMessage", handleAddMessage);
+
+    return () => {
+      socket.off("receiveMessage", handleAddMessage); // Cleanup to avoid duplicates
+    };
+  }, [socket]);
+
+  return (
+    <>
+      {
+        room ? 
+        <div className="room-no">Room number: {room} <button onClick={() => {setRoom(null)}}>Reset</button></div>
+          :
+        <div className="add-room-container">
+        <input type="text" onChange={(e) => {setRoomInput(e.target.value)}} />
+        <button onClick={() => {joinRoom()}}>Join</button>
+      </div>
+      }
+      
+      <div className="message-container">
+        <div className="messages">
+          {
+            !messages || messages.length === 0 ? <div>No Messages</div> 
+            : 
+            messages.filter((val) => {if(val.room === room) {return true}}).map((data, index) => {
+              const {message, user} = data
+                return(
+                  <div className={`message ${user === "me" ? "my-message" : "other-message"}`} key={index}>{message}</div>
+                )
+            })
+          }
+          
+        </div>
+        <input 
+        type="text" 
+        placeholder='Enter ur message'
+        value={messageInput}
+        onChange={(e) => {setMessageInput(e.target.value)}}
+        />
+        <button onClick={() => {sendMessage()}}> Send a message </button>
+      </div>
+    </>
+  )
+}
+
+export default App
